32c54d06eccab2c1eef1f60645ecb3263ba1e840
Thalia Matos <thalia.matos3197@gmail.com>
2025-05-15 15:56:23 -0300

    inicio

--- backend/src/config/auth.ts
+++
@@ -1,5 +0,0 @@
-export default {
-    secret: process.env.JWT_SECRET || 'segredo_super_tia',
-    expiresIn: '1d',
-  };
-  
--- backend/src/routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { register, login } from '../controllers/authController';
-
-const router = Router();
-
-router.post('/auth/register', register);
-router.post('/auth/login', login);
-
-export default router;
